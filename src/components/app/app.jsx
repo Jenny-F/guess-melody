82713b8eefeb0f336f9ae832c3694bd48d92d984
Evgeniya Filatova <jennyfilatova@gmail.com>
2020-09-23 14:08:24 +0400

    add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   const {errorsCount} = props;
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={3}
+            onWelcomeButtonClick={() => {}}
+          />
+        </Route>
+        <Route exact path="/artist">
+          <ArtistQuestionScreen/>
+        </Route>
+        <Route exact path="/genre">
+          <GenreQuestionScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 

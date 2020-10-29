37fff4f0591be98f6121cb1c3f238da4943395da
Evgeniya Filatova <jennyfilatova@gmail.com>
2020-10-29 14:01:59 +0400

    add selectors

---
+++ src/reducer/data/selectors.js
@@ -0,0 +1,29 @@
+import {createSelector} from "reselect";
+import NameSpace from "../../name-space.js";
+
+const NAME_SPACE = NameSpace.DATA;
+
+const getQuestions = (state) => {
+  return state[NAME_SPACE].questions;
+};
+
+const randomFilter = () => {
+  return Math.random() > 0.5;
+};
+
+const getArtistQuestions = createSelector(
+    getQuestions,
+    randomFilter,
+    (resOne, resTwo) => {
+      resOne.filter((item) => resTwo && item.type === `artist`);
+    }
+);
+
+const getGenreQuestions = createSelector(
+    getQuestions,
+    (res) => {
+      res.filter((item) => item.type === `genre`);
+    }
+);
+
+export default {getQuestions, getArtistQuestions, getGenreQuestions};
---
+++ src/reducer/game/selectors.js
@@ -0,0 +1,17 @@
+import NameSpace from "../../name-space.js";
+
+const NAME_SPACE = NameSpace.GAME;
+
+const getMistakesCount = (state) => {
+  return state[NAME_SPACE].mistakesCount;
+};
+
+const getMaxMistakesCount = (state) => {
+  return state[NAME_SPACE].maxMistakesCount;
+};
+
+const getStep = (state) => {
+  return state[NAME_SPACE].step;
+};
+
+export {getMaxMistakesCount, getMistakesCount, getStep};

7448deaf8356155ec99736586861d7338933da93
Evgeniya Filatova <jennyfilatova@gmail.com>
2020-10-29 15:44:28 +0400

    move reducer and name-space

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from "redux";
 import NameSpace from "./name-space.js";
-import {reducer as data} from "./reducer/data/data.js";
-import {reducer as game} from "./reducer/game/game.js";
+import {reducer as data} from "../reducer/data/data.js";
+import {reducer as game} from "../reducer/game/game.js";
 
 export default combineReducers({
   [NameSpace.DATA]: data,
